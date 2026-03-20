8d8ca230cc814e1b483e268a07aee5b66d99a737
Joe Workman <joe@workmanmail.com>
2026-03-20 13:23:20 -0700

    Update proton to v0.8.0

--- Formula/proton.rb
+++ Formula/proton.rb
@@ -1,9 +1,9 @@
 class Proton < Formula
   desc "CLI static site builder using Twig, Markdown, and YAML"
   homepage "https://foundationcss.com/proton"
-  url "https://github.com/foundation/proton/releases/download/VERSION/proton"
-  sha256 "SHA256"
-  version "VERSION"
+  url "https://github.com/foundation/proton/releases/download/v0.8.0/proton"
+  sha256 "6935b9de994409c3704fa8fba5747111979716800f5a85ff81dfd3b707a6441b"
+  version "v0.8.0"
 
   depends_on "php"
 
